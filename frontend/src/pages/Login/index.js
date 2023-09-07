e3bf66e8ad54be5ad4671718787bb8ed41d8aafd
vkmtx <110697534+vkmtx@users.noreply.github.com>
2023-09-06 21:03:35 -0300

    updt

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -35,7 +35,7 @@ const useStyles = makeStyles(theme => ({
 		width: "100vw",
 		height: "100vh",
 		//background: "linear-gradient(to right, #76EE00 , #76EE00 , #458B00)",
-		backgroundImage: "url(https://i.imgur.com/CGby9tN.png)",
+		backgroundImage: "url(https://i.imgur.com/DbjqFnP.jpg)",
 		backgroundRepeat: "no-repeat",
 		backgroundSize: "100% 100%",
 		backgroundPosition: "center",
