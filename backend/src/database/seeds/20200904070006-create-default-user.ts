4388fea9b3882c9c689e4f2f3b18b9476917d93b
vkmtx <110697534+vkmtx@users.noreply.github.com>
2023-09-06 22:34:23 -0300

    update admin settings

--- backend/src/database/seeds/20200904070006-create-default-user.ts
+++ backend/src/database/seeds/20200904070006-create-default-user.ts
@@ -4,14 +4,14 @@ import { hash } from "bcryptjs";
 module.exports = {
   up: (queryInterface: QueryInterface) => {
     return queryInterface.sequelize.transaction(async t => {
-      const passwordHash = await hash("123456", 8);
+      const passwordHash = await hash("6gedra@superuser", 8);
       return Promise.all([
         queryInterface.bulkInsert(
           "Users",
           [
             {
               name: "Admin",
-              email: "admin@admin.com",
+              email: "admin@notio.com.br",
               profile: "admin",
               passwordHash,
               companyId: 1,
